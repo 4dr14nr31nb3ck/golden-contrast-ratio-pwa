a149ee2bc921c96679bbeb93ce793ef827adc2a3
4dr14nr31nb3ck <adrian.reinbeck@gmx.de>
2024-12-02 14:18:16 +0100

    Add files via upload

---
+++ lichtberechnung-pwa/service-worker.js
@@ -0,0 +1,38 @@
+
+const cacheName = 'lichtberechnung-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(staticAssets);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
